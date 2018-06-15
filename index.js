bb23e4cc0ae26da8f994c0ffeba4b8f0b8b5660d
rbairwell <rbairwell@users.noreply.github.com>
2018-06-15 11:42:52 +0100

    Working now

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var scriptPath = __dirname+'/build/HTMLCS.js';
 
 if (fs.existsSync(scriptPath) === true) {
     module.exports={
-        library:function() { require(scriptPath) },
-        source:function () { return fs.readFileSync(scriptPath); }
+        library:require(scriptPath),
+        source:fs.readFileSync(scriptPath)
     };
 } else {
     throw new Error('HTMLCS must be built using `grunt build` before it can be required in Nodejs.');
