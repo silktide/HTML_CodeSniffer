6ae9a16bc3c35e0e745d1d4cdf8d06b1c941251c
Doug Nelson <dougnelson@silktide.com>
2019-06-18 14:09:36 +0100

    updated pre-push-hook

--- pre-push-hook.sh
+++ pre-push-hook.sh
@@ -8,7 +8,7 @@ fi
 git checkout build/HTMLCS.js
 node_modules/.bin/grunt build-debug
 
-if git diff --exit-code build/HTMLCS.js; then
+if ! git diff --exit-code build/HTMLCS.js; then
     git add build/HTMLCS.js
     git commit -m "updated build"
 fi
